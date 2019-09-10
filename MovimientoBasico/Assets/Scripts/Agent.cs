52f46f6c82acddb920559e325f2b269f42e1b7ce
Angelo Acosta <angelocastellanos99@gmail.com>
2019-09-09 21:25:22 -0500

    commit

---
+++ MovimientoBasico/Assets/Scripts/Agent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+    Vector3 steer = Vector3.zero;
+    
+
+    float maxSpeed = 5;
+    float maxSteer = 1;
+
+    Transform target;
+ 
+   
+    void Update()
+    {
+        target = GameObject.Find("Target").transform;
+        //desired
+        desired = (target.position - transform.position).normalized * maxSpeed;
+
+        //steer
+        steer = Vector3.ClampMagnitude(desired - velocity, maxSteer);
+
+        velocity = steer * Time.deltaTime;
+        transform.position -=velocity;
+
+        if(transform.position.x <= 0.5 & transform.position.x >= -0.5)
+        {
+            Debug.Log("¡You win!");
+            Destroy(gameObject);
+        }else if(transform.position.y <= 0.3 & transform.position.y > -0.3)
+        {
+            Debug.Log("¡You win");
+            Destroy(gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+        Debug.Log("You Lose");
+    }
+}
---
+++ MovimientoBasico/Assets/Scripts/Target.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+   float tx = 10;
+   float ty = 20;
+   float minX = -5;
+   float minY = -5;
+   float width = 10;
+   float height = 10;
+
+
+    void Update()
+    {
+         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.Scale(new Vector3(1, 1, 0));
+        transform.position = mousePosition; 
+    }
+}
